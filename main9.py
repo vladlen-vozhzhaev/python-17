312f4e1eb632df78e084a298780b0098ad6d504c
Vlalden <vladlen@vozhzhaev.ru>
2024-06-17 11:40:04 +0300

    mysql

---
+++ main9.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+config = {
+    'user': 'root',
+    'password': '',
+    'host': '127.0.0.1',
+    'database': 'test17'
+}
+
+conn = mysql.connector.connect(**config)
+
+cursor = conn.cursor()
+name = input("Введите имя: ")
+login = input("Введите логин: ")
+password = input("Введите пароль: ")
+cursor.execute(f"INSERT INTO `users`(`name`, `login`, `pass`) VALUES ('{name}','{login}','{password}')")
+conn.commit()
+# login = input("Введите логин: ")
+# password = input("Введите пароль: ")
+# data = (login, password)
+# cursor.execute("SELECT * FROM users WHERE login = %s AND pass = %s", data)
+# for row in cursor.fetchall():
+#     print(row)
+#cursor.close()
+#conn.close()
--- matrix.py
+++ matrix.py
@@ -14,17 +14,22 @@ nums = [
     [6,2],
     [7,1]
 ]
+result = ""
+for i in range(len(nums)):
+    for j in range(len(nums[i])):
+        result += str(nums[i][j])
 
-def transpose_matrix(arr):
-    n = len(arr)
-    transposed = [ [0]*n for _ in range(n) ]
-
-    for i in range(n):
-        for j in range(len(arr[i])):
-            transposed[j][i] = arr[i][j]
-    return transposed
-
-print( transpose_matrix(nums) )
+print(result)
+# def transpose_matrix(arr):
+#     n = len(arr)
+#     transposed = [ [0]*n for _ in range(n) ]
+#
+#     for i in range(n):
+#         for j in range(len(arr[i])):
+#             transposed[j][i] = arr[i][j]
+#     return transposed
+#
+# print( transpose_matrix(nums) )
 
 # # Поиск максимального элемента
 # max = num[0][0]
